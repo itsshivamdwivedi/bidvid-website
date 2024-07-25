b26d2b111969401bf8affefe0028974a8902f07d
iamshivamdwivedi410 <iamshivamdwivedi410@gmail.com>
2024-07-25 18:26:37 +0530

    nav_mobile

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="bg-white p-4 shadow-md flex justify-between items-center">
-      <div className="flex items-center lg:ml-12"> {/* Added padding-left for large screens */}
-        <Image src="/logo.png" alt="BidVid Logo" width={172} height={86} />
+      <div className="flex items-center lg:ml-12 "> {/* Added padding-left for large screens */}
+        <Image src="/logo.png" alt="BidVid Logo" width={102} height={49} />
       </div>
       <div className="space-x-4 lg:pr-6"> {/* Added padding-right for large screens */}
         <Link href="#about-us" legacyBehavior>
