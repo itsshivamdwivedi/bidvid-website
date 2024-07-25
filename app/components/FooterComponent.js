9899a232136f5b293f90b527c92b2e21031ba1c4
iamshivamdwivedi410 <iamshivamdwivedi410@gmail.com>
2024-07-25 17:41:01 +0530

    commit

--- app/components/FooterComponent.js
+++ app/components/FooterComponent.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const FooterComponent = () => {
     return (
-        <footer className="bg-gray-900 text-white h-32 md:h-auto flex flex-col justify-between p-4 md:p-6">
-            <div className="flex flex-col md:flex-row justify-between items-center">
+        <footer className="bg-gray-900 text-white h-32 md:h-auto flex flex-col md:flex-col justify-between p-4 md:p-6">
+            {/* Aligning content for mobile and larger screens */}
+            <div className="flex flex-row-reverse justify-between items-center md:flex-row md:justify-between">
                 <img 
                     src="/logo-bg-removed.png" 
                     alt="BidVid logo" 
-                    className="w-20 h-auto mb-4 md:mb-0 md:w-28 md:ml-4 lg:w-40 lg:ml-24 xl:ml-28" 
+                    className="w-20 h-auto md:w-28 md:ml-4 lg:w-40 lg:ml-24 xl:ml-28" 
                 />
-                <div className="flex flex-col items-center space-y-2 md:space-y-0 md:space-x-4 md:flex-row">
+                <div className="flex space-x-4">
+                    {/* Hide 'Contact Us' on mobile */}
                     <a href="#" className="hidden md:inline hover:underline">Contact Us</a>
                     <a href="#" className="hover:underline">About Us</a>
                 </div>
