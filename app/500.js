48d89ec8b70a127f153ab9a60939726c410446a2
iamshivamdwivedi410 <iamshivamdwivedi410@gmail.com>
2024-07-25 14:29:50 +0530

    commit

--- app/500.js
+++ app/500.js
@@ -3,20 +3,8 @@
 import React from 'react';
 
 const ErrorPage = ({ errorCode }) => {
-  if (errorCode === 404) {
-    return <h1>404 - Page Not Found</h1>;
-  }
-
-  if (errorCode === 500) {
-    return <h1>500 - Server-side Error</h1>;
-  }
 
   return <h1>Unexpected Error</h1>;
-};
-
-ErrorPage.getInitialProps = ({ res, err }) => {
-  const errorCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { errorCode };
 };
 
 export default ErrorPage;
---
+++ app/components/[...not-found]/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 -This Page is Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
