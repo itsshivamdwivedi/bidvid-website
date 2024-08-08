8b2caf76f3b082f67c0d351ff1765c6995f94ba0
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-08-08 19:33:31 +0000

    commit

--- app/page.js
+++ app/page.js
@@ -1,20 +1,26 @@
 'use client';
 
 import React, { useState } from 'react';
-import Image from 'next/image';
+
 import { Input, Textarea, Button } from '@chakra-ui/react';
 import HeroSection from './components/HeroSection';
 import Services from './components/Services';
 import Benefits from './components/Benefits';
 import FooterComponent from './components/FooterComponent';
 import './components/ContactForm.css';
-import ExportedImage from "next-image-export-optimizer";
-import Link from 'next/link';
 import Contact from "./contact/page";
+import Head from 'next/head';
 
 export default function Page(){
   return (
     <>
+     <Head>
+        <title>BidVids: Revolutionising Bidding with AI-Powered Optimisation</title>
+        <meta name="description" content="BidVids is a cutting-edge ad technology platform that uses advanced AI algorithms to optimise advertising yield for digital media buyers. Boost revenue and enhance user experience seamlessly." />
+        <meta property="og:title" content="BidVids: Revolutionising Bidding with AI-Powered Optimisation" />
+        <meta property="og:description" content="BidVids is a cutting-edge ad technology platform that uses advanced AI algorithms to optimise advertising yield for digital media buyers. Boost revenue and enhance user experience seamlessly." />
+      
+      </Head>
       <HeroSection />
       <Services />
       <Benefits />
