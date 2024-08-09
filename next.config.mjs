01bc166d3445755adc1e2925a5bf3e2b062e0667
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-08-09 06:10:42 +0000

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 export default {
     
     distDir: 'dist',
-    output: 'export',
-    reactStrictMode: true,
+    // output: 'export',
+    // reactStrictMode: true,
     images: {
         loader: "custom",
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
